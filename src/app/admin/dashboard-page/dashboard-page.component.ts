b3d621b7ed63132c7de06211499266234b2f7669
Andrei Hincu <a.hincu@octopod.ru>
2020-02-24 17:12:22 +0200

    Вывод списка постов

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PostService } from '../shared/services/post.service';
+import { Post } from '../../shared/interfaces';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  pSub: Subscription;
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.pSub = this.postService.getAll().subscribe(res => this.posts = res);
   }
 
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
+
+  remove(id: string) {
+
+  }
 }
